c38c284272f1ec4aa55e0b2cc94be256d19cc715
Jutinapas Klangcharoenkul <jutinapas@gmail.com>
2018-12-13 16:21:52 +0700

    init xcode project

---
+++ HeroRescueOfficerApp/HeroRescueOfficerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeroRescueOfficerApp
+//
+//  Created by CNC on 13/12/2561 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
